3b44f48c817abc2bd718e6ee1dd527050d7bc2e0
omtagline <omm.tagline@gmail.com>
2024-10-04 16:27:10 +0530

    env change

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-// API:'https://demo-chat-backend.onrender.com'
-API:'http://localhost:8000'
+API:'https://demo-chat-backend.onrender.com'
+// API:'http://localhost:8000'
 };
